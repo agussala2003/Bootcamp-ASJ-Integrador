b4c92e9938b21860404b179d6a2c3c85ba80f20c
agussala2003 <asaladino@petersen.corp>
2024-01-13 20:27:18 -0300

    Agregamos Landing

--- 20231215-23-Saladino-Angular/src/app/components/navigation-home/navigation-home.component.ts
+++ 20231215-23-Saladino-Angular/src/app/components/navigation-home/navigation-home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-home',
   templateUrl: './navigation-home.component.html',
   styleUrl: './navigation-home.component.css'
 })
-export class NavigationHomeComponent {
-
+export class NavigationHomeComponent implements OnInit {
+  valor:any = true;
+  ngOnInit(): void {
+    const location = (window.location.pathname === '/inicio')
+    if (!location) {
+      this.valor = JSON.parse(
+        localStorage.getItem('inicio') || 'null'
+      );
+      this.valor !== null ? this.valor : null;
+    }
+  }
 }
